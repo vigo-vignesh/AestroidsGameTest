28bb759c6226c36c35c692ba2e406fb265b3c911
Vignesh_G <41943640+vigo-vignesh@users.noreply.github.com>
2020-06-01 08:14:11 +0530

    Adjusted the movement speed

--- AestroidGameUnityFiles/Assets/Scripts/SpaceShipController.cs
+++ AestroidGameUnityFiles/Assets/Scripts/SpaceShipController.cs
@@ -7,7 +7,7 @@ public class SpaceShipController : MonoBehaviour
     public Rigidbody playerRigidBody;
     private float verticalInput;
 
-    [Range(0f, 5f)]
+    [Range(10f, 15f)]
     public float playerSpeed;
 
     private Vector3 target;
@@ -21,6 +21,7 @@ public class SpaceShipController : MonoBehaviour
         transform.GetChild(0).rotation = Quaternion.Euler(0, 0, rotationZ);
 
         SpaceShipMovement();
+        CheckPosition();
     }
     void SpaceShipMovement()
     {
@@ -30,5 +31,35 @@ public class SpaceShipController : MonoBehaviour
             Vector2 mousePointerPosition = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
             playerRigidBody.AddForce(mousePointerPosition * playerSpeed);
         }
+    }
+
+    private void CheckPosition()
+    {
+
+        float sceneWidth = Camera.main.orthographicSize * 2 * Camera.main.aspect;
+        float sceneHeight = Camera.main.orthographicSize * 2;
+
+        float sceneRightEdge = sceneWidth / 2;
+        float sceneLeftEdge = sceneRightEdge * -1;
+        float sceneTopEdge = sceneHeight / 2;
+        float sceneBottomEdge = sceneTopEdge * -1;
+
+        if (transform.GetChild(0).position.x > sceneRightEdge)
+        {
+            transform.GetChild(0).position = new Vector2(sceneLeftEdge, transform.GetChild(0).position.y);
+        }
+        if (transform.GetChild(0).position.x < sceneLeftEdge)
+        {
+            transform.GetChild(0).position = new Vector2(sceneRightEdge, transform.GetChild(0).GetChild(0).position.y);
+        }
+        if (transform.GetChild(0).position.y > sceneTopEdge)
+        {
+            transform.GetChild(0).position = new Vector2(transform.GetChild(0).position.x, sceneBottomEdge);
+        }
+        if (transform.GetChild(0).position.y < sceneBottomEdge)
+        {
+            transform.GetChild(0).position = new Vector2(transform.GetChild(0).position.x, sceneTopEdge);
+        }
+
     }
 }
